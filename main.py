c46581ca1907775b623bc446b9302446c5494f19
nattakits <40267160+nattakits@users.noreply.github.com>
2020-08-29 22:43:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,23 @@ import time
 import os
 from os import environ
 
-print('hi')
+
+INTERVAL = 5 * 60 * 1  # tweet every 6 hours
+# INTERVAL = 15  # every 15 seconds, for testing
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+consumer_key = environ['CONSUMER_KEY']
+consumer_secret = environ['CONSUMER_SECRET']
+key = environ['ACCESS_KEY']
+secret = environ['ACCESS_SECRET']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(key, secret)
+api = tweepy.API(auth)
+
+INTERVAL = 5 * 60 * 1  # tweet every 6 hours
+
+while True:
+    print("about to get ad...")
+    api.update_status("hello Test")
