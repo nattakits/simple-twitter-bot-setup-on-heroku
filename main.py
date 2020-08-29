eed8160727793ffe61928f03e40e40772db57482
nattakits <40267160+nattakits@users.noreply.github.com>
2020-08-29 22:24:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,33 @@
 ####################
 #
 #
-# Write Bot Code In This File
+from os import environ
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+
+INTERVAL = 5 * 60 * 1  # tweet every 6 hours
+# INTERVAL = 15  # every 15 seconds, for testing
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+while True:
+    print("about to get ad...")
+    api.update_status("hello Test")
+    time.sleep(INTERVAL)
+
+
+
+
+
+
+
+
+
 #
 ####################
 ####################
