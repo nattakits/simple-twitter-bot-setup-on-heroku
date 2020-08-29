75afc86d18279a5ef33b5fbe03bc205c6e24c8bb
nattakits <40267160+nattakits@users.noreply.github.com>
2020-08-29 22:28:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,30 @@
 ####################
 #
 #
+
+import tweepy
+import logging
+from time import sleep
+import time
+import os
 from os import environ
 
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']
+logging.basicConfig()
+logger = logging.getLogger("BOT")
+logger.setLevel(logging.DEBUG)
+
+
+consumer_key = environ['CONSUMER_KEY']
+consumer_secret = environ['CONSUMER_SECRET']
+key = environ['ACCESS_KEY']
+secret = environ['ACCESS_SECRET']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(key, secret)
+api = tweepy.API(auth)
+
 
 INTERVAL = 5 * 60 * 1  # tweet every 6 hours
-# INTERVAL = 15  # every 15 seconds, for testing
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
 
 while True:
     print("about to get ad...")
